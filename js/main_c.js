b532e3e72a6d32e2e1a5fafb57c4f83c14392dae
GustavoRivarola <139401219+GustavoRivarola@users.noreply.github.com>
2023-08-16 22:20:20 -0300

    TP 6 se hace commit #3
    Se realiza cambio a uno de los archivos

--- js/main_c.js
+++ js/main_c.js
@@ -1,9 +1,11 @@
 var cantidadDeGatos = prompt('EJERCICIO 6 Punto c - Ingrese cantidad de gatos')
 var cantidadDePasos = prompt('EJERCICIO 6 Punto c - Ingrese cantidad de pasos')
 var pasos = []
+
 console.log('EJERCICIO 6 Punto c - Ingrese cantidad de gatos y cantidad de pasos')
 console.log('cantidadDeGatos: ' + cantidadDeGatos)
 console.log('cantidadDePasos: ' + cantidadDePasos)
+
 for(var p = 1; p <= cantidadDePasos;  p++) {
     pasos[p] = '🐾'
 }
